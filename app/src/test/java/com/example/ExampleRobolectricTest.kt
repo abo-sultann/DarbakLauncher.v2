8472ec2220ae5829cc03a52505631f94b638a92e
abo-sultann <abo-sultann@hotmail.com>
2026-09-18 11:00:56 +0300

    Align side-by-side app name test

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("Launcher 2026", appName)
+    assertEquals("Darbak Launcher v2 WIDGET TEST", appName)
   }
 }
